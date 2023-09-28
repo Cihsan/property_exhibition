8e6ab125346737bd044ec73946f504a84b46a2b2
Obaidur1 <obaidur0960@gmail.com>
2023-09-29 01:38:39 +0600

    Login Logout Registration Done

--- Property_Exhibition/accounts/admin.py
+++ Property_Exhibition/accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account, UserProfile
-from django.utils.html import format_html
+from .models import Account
+
 
 # Register your models here.
 
@@ -11,6 +11,8 @@ class AccountAdmin(UserAdmin):
         "email",
         "first_name",
         "last_name",
+        "phone_number",
+        "city",
         "last_login",
         "date_joined",
         "is_active",
@@ -24,22 +26,3 @@ class AccountAdmin(UserAdmin):
 
 
 admin.site.register(Account, AccountAdmin)
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    def thumbnail(self, object):
-        if object.profile_picture:
-            return format_html(
-                '<img src="{}" width="30" style="border-radius:50%;">'.format(
-                    object.profile_picture.url
-                )
-            )
-        return format_html(
-            '<img src="https://www.vecteezy.com/vector-art/2002403-man-with-beard-avatar-character-isolated-icon" width="30" style="border-radius:50%;">'
-        )
-
-    thumbnail.short_description = "Profile Picture"
-    list_display = ("thumbnail", "user", "city", "state", "country")
-
-
-admin.site.register(UserProfile, UserProfileAdmin)
--- Property_Exhibition/accounts/views.py
+++ Property_Exhibition/accounts/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate, login
+from .models import Account
 
 
 # Create your views here.
 def register(request):
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        username = email.split("@")[0]
+        password = request.POST["password"]
+        password_confirmation = request.POST["password_confirmation"]
+        user = Account.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            username=username,
+            password=password,
+        )
+        user.save()
+        return redirect("login")
     return render(request, "register.html")
 
 
@@ -16,7 +33,9 @@ def user_login(request):
     if request.method == "POST":
         email = request.POST["email"]
         password = request.POST["password"]
-        user = authenticate(request, username=email, password=password)
+        print(email, password)
+        user = authenticate(request, email=email, password=password)
+        print(user)
         if user is not None:
             login(request, user)
             print("login success")
