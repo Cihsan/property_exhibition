e799055be7225ae8337db8ff6c3928c09c385cba
Obaidur1 <obaidur0960@gmail.com>
2023-09-29 00:15:44 +0600

    User model

--- Property_Exhibition/accounts/views.py
+++ Property_Exhibition/accounts/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render,redirect 
+from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from .forms import LoginForm
+
 
 # Create your views here.
 def register(request):
     return render(request, "register.html")
 
+
 def update_profile(request):
     return render(request, "update_profile.html")
 
+
 def user_login(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        print(email)
     return render(request, "signin.html")
+
 
 def user_logout(request):
     logout(request)
-    return redirect('login') 
+    return redirect("login")
