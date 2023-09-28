1b8b7c1520590254bae9604cb6757b66980279db
Nazrul islam <nazrulislamsuton06@gmail.com>
2023-09-28 23:37:54 +0600

    some-updated

--- Property_Exhibition/Property_Exhibition/urls.py
+++ Property_Exhibition/Property_Exhibition/urls.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path,include
 from . import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path('',views.home, name='home'),
-=======
-from django.urls import include, path
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('',views.home, name='home'),
     path("", include("accounts.urls")),
     path("all_property/", include("all_property.urls")),
     path("dashboard/", include("dashboard.urls")),
->>>>>>> 064ecd0bac1c4bdc441c1e923a057c16b0653b9f
 ]
