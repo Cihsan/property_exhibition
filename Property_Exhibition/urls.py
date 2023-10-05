3ae658c7ea7e7c10e7c2268f54a8b8897c7f3d5a
tohidrupok <pcgamertohid@gmail.com>
2023-10-05 18:28:43 +0600

    google facebook add

--- Property_Exhibition/urls.py
+++ Property_Exhibition/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path,include
-from . import views
+from . import views 
+from django.views.generic import TemplateView
+from accounts import templates
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/', include('allauth.urls')), 
+     
+     
     path('',views.home, name='home'),
     path("", include("accounts.urls")),
     path("all_property/", include("all_property.urls")),
