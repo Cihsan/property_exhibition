27cd121060096f4fdc54631c40ad2e0d2c9615d6
Obaidur1 <obaidur0960@gmail.com>
2023-10-06 13:27:57 +0600

    Image handle

--- all_property/admin.py
+++ all_property/admin.py
@@ -1,16 +1,36 @@
 from django.contrib import admin
+
 # Register your models here.
-from .models import Property,Location, PropertyImage
+from .models import Property, PropertyImage
+
 
 # Register your models here.
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('title','type', 'price', 'area', 'purpose','completion','status','created_at', 'updated_at')
-    list_filter = ('type', 'status', 'purpose')
-    search_fields = ('title', 'description', 'type', 'purpose')
+    list_display = (
+        "title",
+        "property_type",
+        "price",
+        "area",
+        "purpose",
+        "completion",
+        "status",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("status", "property_type", "purpose")
+    search_fields = ("title", "property_type", "description", "purpose")
+
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('division','district', 'thana_police_station', 'upazila', 'union_ward','village')
+    list_display = (
+        "division",
+        "district",
+        "thana_police_station",
+        "upazila",
+        "union_ward",
+        "village",
+    )
+
 
 admin.site.register(Property, PropertyAdmin)
-admin.site.register(Location, LocationAdmin)
-admin.site.register(PropertyImage,admin.ModelAdmin)
+admin.site.register(PropertyImage, admin.ModelAdmin)
--- all_property/models.py
+++ all_property/models.py
@@ -1,50 +1,54 @@
 from django.db import models
+from .bdlocation import location
 
 
 class Property(models.Model):
     title = models.CharField(max_length=255)
     area = models.DecimalField(max_digits=10, decimal_places=2)
-    price = models.DecimalField(max_digits=15, decimal_places=2)
+    price = models.IntegerField()
     description = models.TextField(blank=True, null=True)
-    type = models.CharField(max_length=50)
+    property_type = models.CharField(
+        max_length=50
+    )  # Renamed 'type' to 'property_type' to avoid conflicts with Python reserved word
     purpose = models.CharField(
         max_length=10,
         choices=[
-            ('sell', 'Sell'),
-            ('rent', 'Rent'),
-        ]
+            ("sell", "Sell"),
+            ("rent", "Rent"),
+        ],
     )
     completion = models.CharField(
-        max_length=20,
+        max_length=30,
         choices=[
-            ('all', 'All'),
-            ('ready', 'Ready'),
-            ('unready', 'Unready'),
-            ('under_construction', 'Under Construction'),
-        ]
+            ("all", "All"),
+            ("ready", "Ready"),
+            ("unready", "Unready"),
+            ("under_construction", "Under Construction"),
+        ],
     )
     status = models.CharField(
         max_length=10,
         choices=[
-            ('booked', 'Booked'),
-            ('available', 'Available'),
+            ("booked", "Booked"),
+            ("available", "Available"),
         ],
-        default='available'
+        default="available",
     )
+    division = models.CharField(max_length=80, choices=location.Division, null=True)
+    district = models.CharField(max_length=50, choices=location.District, null=True)
+    upozila = models.CharField(max_length=50, choices=location.Upozila, null=True)
+    union_ward = models.CharField(max_length=50, null=True)
+    village = models.CharField(max_length=50, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
-    
+
+
 class PropertyImage(models.Model):
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
-    img_url = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="property/", default=None)
 
-class Location(models.Model):
-    division = models.CharField(max_length=255)
-    district = models.CharField(max_length=255)
-    thana_police_station = models.CharField(max_length=255)
-    upazila = models.CharField(max_length=255)
-    union_ward = models.CharField(max_length=255)
-    village = models.CharField(max_length=255)
-    property = models.OneToOneField(Property, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.property.title
