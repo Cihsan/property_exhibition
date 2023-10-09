1da74df3f7f5efbc6dc819ccfb4fa1e80ec1952c
Obaidur1 <obaidur0960@gmail.com>
2023-10-09 15:09:30 +0600

    Property model updated according to data.json

--- all_property/models.py
+++ all_property/models.py
@@ -3,13 +3,12 @@ from .bdlocation import location
 
 
 class Property(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     area = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.IntegerField()
     description = models.TextField(blank=True, null=True)
-    property_type = models.CharField(
-        max_length=50
-    )  # Renamed 'type' to 'property_type' to avoid conflicts with Python reserved word
+    property_type = models.CharField(max_length=50)
     purpose = models.CharField(
         max_length=10,
         choices=[
@@ -22,7 +21,7 @@ class Property(models.Model):
         choices=[
             ("all", "All"),
             ("ready", "Ready"),
-            ("unready", "Unready"),
+            ("incomplete", "Incomplete"),
             ("under_construction", "Under Construction"),
         ],
     )
@@ -37,6 +36,7 @@ class Property(models.Model):
     division = models.CharField(max_length=80, choices=location.Division, null=True)
     district = models.CharField(max_length=50, choices=location.District, null=True)
     upozila = models.CharField(max_length=50, choices=location.Upozila, null=True)
+    img_url = models.URLField(max_length=300)
     union_ward = models.CharField(max_length=50, null=True)
     village = models.CharField(max_length=50, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -47,11 +47,3 @@ class Property(models.Model):
 
     def address(self):
         return f"{self.district}, {self.division}"
-
-
-class PropertyImage(models.Model):
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="property/", default=None)
-
-    def __str__(self):
-        return self.property.title
