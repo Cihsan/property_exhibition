85b71b679778ed8fe3113d0d0dc7008b19b91ca9
Obaidur1 <obaidur0960@gmail.com>
2023-10-09 21:54:22 +0600

    Database POSTGRE added

--- Property_Exhibition/settings.py
+++ Property_Exhibition/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,10 +122,10 @@ WSGI_APPLICATION = "Property_Exhibition.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgres://property_exhibition_user:1LWFagZ1OqJZTVZRDnrHURbOSnTMjcvz@dpg-cki1vekldqrs73faacug-a.singapore-postgres.render.com/property_exhibition",
+        conn_max_age=600,
+    )
 }
 
 
