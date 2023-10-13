b52e98093b977051f7e847e98e3bcec7b371ce51
Obaidur1 <obaidur0960@gmail.com>
2023-10-13 23:54:15 +0600

    Response user profile id

--- accounts/views.py
+++ accounts/views.py
@@ -31,9 +31,12 @@ class LoginAPIView(APIView):
 
             if user is not None:
                 login(request, user)
+                user_profile = UserProfile.objects.get(user=user)
+
                 token, created = Token.objects.get_or_create(user=user)
                 return Response(
-                    {"token": token.key, "user_id": user.id}, status=status.HTTP_200_OK
+                    {"token": token.key, "profile_id": user_profile.id},
+                    status=status.HTTP_200_OK,
                 )
             else:
                 return Response(
