fb7454a8ba25405455b8a6df16413efc500b674a
Obaidur1 <thetutorlab360@gmail.com>
2023-10-21 21:47:00 +0600

    User and likes added

--- all_property/models.py
+++ all_property/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Property(models.Model):
     id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=25)
     title = models.CharField(max_length=255)
     area = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.IntegerField()
@@ -22,6 +24,7 @@ class Property(models.Model):
     village = models.CharField(max_length=50, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
